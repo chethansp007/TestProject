024b43468f421cb1097dca01a9df35a7e3b7d563
Chethan <Chethan.palaksha@tigerspike.com>
2016-08-11 15:24:21 +0800

    Initial commit

---
+++ DemoApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by chethan.palaksha on 11/8/16.
+//  Copyright © 2016 TigerSpike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var descLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
