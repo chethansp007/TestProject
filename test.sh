8d52bfc90a77dfd22812456c3bb9e10fd215c36f
Chethan <Chethan.palaksha@tigerspike.com>
2016-08-15 13:52:27 +0800

    Scrtipt to push build.

---
+++ test.sh
@@ -0,0 +1,4 @@
+xcodebuild -scheme DemoApp -archivePath builds/DemoApp.xcarchive archive
+
+export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer/
+xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath builds/DemoApp.xcarchive -exportPath builds
