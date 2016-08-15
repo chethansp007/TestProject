f0e3e95e8a400c1a8ad0d41bea34f16e72dbba92
Chethan <Chethan.palaksha@tigerspike.com>
2016-08-15 18:11:39 +0800

    Version Bump

--- test.sh
+++ test.sh
@@ -2,3 +2,7 @@ xcodebuild -scheme DemoApp -archivePath builds/DemoApp.xcarchive archive
 
 export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer/
 xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath builds/DemoApp.xcarchive -exportPath builds
+
+git add .
+git commit -m "Version Bump"
+git push
