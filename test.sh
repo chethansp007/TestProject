0ee6dee79285870518afb3d8c0866e329c768021
Chethan <Chethan.palaksha@tigerspike.com>
2016-08-17 20:56:09 +0800

    Included the script

--- test.sh
+++ test.sh
@@ -1,28 +1,8 @@
-if [ $CONFIGURATION == Release ]; then
-echo "Bumping build number..."
-plist=${PROJECT_DIR}/${INFOPLIST_FILE}
-
-# increment the build number (ie 115 to 116)
-buildnum=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${plist}")
-if [[ "${buildnum}" == "" ]]; then
-echo "No build number in $plist"
-exit 2
-fi
-
-buildnum=$(expr $buildnum + 1)
-/usr/libexec/Plistbuddy -c "Set CFBundleVersion $buildnum" "${plist}"
-echo "Bumped build number to $buildnum"
-
-else
-echo $CONFIGURATION " build - Not bumping build number."
-fi
-
-
 xcodebuild -scheme DemoApp -archivePath builds/DemoApp.xcarchive archive
 
 export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer/
 xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath builds/DemoApp.xcarchive -exportPath builds
 
-git add .
-git commit -m "Version Bump"
-git push
+# git add .
+# git commit -m "Version Bump"
+# git push
